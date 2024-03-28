44d29da034c70dade650469152a28f83c78c427b
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-28 17:48:06 +0700

    void displayy

--- QuickSortt/QuickSortt.cpp
+++ QuickSortt/QuickSortt.cpp
@@ -77,9 +77,19 @@ void q_short(int low, int high)
 }
 
 void display() {
+    cout << "\n--------------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "\n--------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
 
 }
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
