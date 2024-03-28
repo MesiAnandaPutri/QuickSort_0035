b3a83bc3a125b875b8f7424fdd0f0f88a7564b99
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-28 17:31:22 +0700

    step 7

--- QuickSortt/QuickSortt.cpp
+++ QuickSortt/QuickSortt.cpp
@@ -57,6 +57,11 @@ void q_short(int low, int high)
             i++;
             cmp_count++;
         }
+        cmp_count++;
+        while ((arr[j] > pivot) && (j >= low))
+        {
+
+        }
     }
 }
 
