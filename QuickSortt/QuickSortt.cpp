5f8a6ee012137d70e70d8bc375702093193eb1f0
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-28 16:48:38 +0700

    menyimpan nilai

--- QuickSortt/QuickSortt.cpp
+++ QuickSortt/QuickSortt.cpp
@@ -2,6 +2,13 @@
 //
 
 #include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
+int n;
 
 int main()
 {
